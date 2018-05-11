2d917168703851bb1e0e4cf2f3aa01f6044b0c17
llHoYall <hoya128@gmail.com>
2018-05-11 16:18:02 +0900

    Complete application

---
+++ TabBar_iOS/ViewController2.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController2.swift
+//  TabBar_iOS
+//
+//  Created by hoya kim on 11/05/2018.
+//  Copyright © 2018 ChameleoN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+	@IBOutlet weak var outputLabel: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	/*
+	// MARK: - Navigation
+
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+			// Get the new view controller using segue.destinationViewController.
+			// Pass the selected object to the new view controller.
+	}
+	*/
+	
+	@IBAction func buttonAction(_ sender: Any) {
+		outputLabel.text = "Pressed button 2"
+	}
+}
